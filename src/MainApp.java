022c4ccc0045cdd396b6cf5099b8aabc9384b93f
DocentSzachista <57325417+DocentSzachista@users.noreply.github.com>
2020-07-30 10:54:58 +0200

    Update MainApp.java
    Adding some methods to make code more readable

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,11 +7,31 @@ public class MainApp {
     {
         FracOperations equations;
         Scanner input= new Scanner(System.in);
+        equations=new FracOperations(GetFractionFromString.separate(inputNumbers(input)), GetFractionFromString.separate(inputNumbers(input)));
         print();
         int wybor= input.nextInt();
-        //equations=new FracOperations( inputNumbers(input), inputNumbers(input) );
-        equations=new FracOperations(GetFractionFromString.separate("-1/-2"), GetFractionFromString.separate("1/2"));
-        switch (wybor)
+        menu(wybor, equations);
+
+    }
+    private static void print()
+    {
+        System.out.println( "Jaka operacje chcesz wykonac?" );
+        System.out.println( "1. Dodawanie" );
+        System.out.println( "2. Odejmowanie" );
+        System.out.println( "3. Mnożenie" );
+        System.out.println( "4. Dzielenie" );
+        System.out.println( "5. Wyjdź." );
+    }
+    private static String inputNumbers(Scanner input)
+    {
+        String fraction;
+        System.out.println( "Podaj ułamek a/b oddzielając licznik od mianownika znakiem '/'" );
+        fraction=input.nextLine();
+        return fraction;
+    }
+    private static void menu(int choice, FracOperations equations)
+    {
+        switch (choice)
         {
             case 1:
             {
@@ -35,29 +55,12 @@ public class MainApp {
             }
             case 5:
                 System.exit(0);
-            default: {
-                System.out.println("Błędny numer operacji został podany");
+            default:
+            {
+                System.out.println("Błędny numer operacji został podany.");
                 break;
             }
 
         }
-    }
-    private static void print()
-    {
-        System.out.println("Jaka operacje chcesz wykonac?");
-        System.out.println("1. Dodawanie");
-        System.out.println("2. Odejmowanie");
-        System.out.println("3. Mnożenie");
-        System.out.println("4. Dzielenie");
-        System.out.println("5. Wyjdź.");
-    }
-    private static Fractions inputNumbers(Scanner input)
-    {
-        int firstNumber, secondNumber;
-        System.out.println("Podaj licznik");
-        firstNumber=input.nextInt();
-        System.out.println("Podaj mianownik");
-        secondNumber=input.nextInt();
-        return new Fractions(firstNumber, secondNumber);
     }
 }
